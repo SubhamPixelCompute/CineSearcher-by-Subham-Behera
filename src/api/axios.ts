d8923dfcf7177c5a314fa3f3f8aa620f03871c37
Subham15112001 <sinchan889377888@gmail.com>
2025-06-17 11:29:51 +0530

    fix url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://www.omdbapi.com/?",
+  baseURL: "https://www.omdbapi.com/?",
   headers: { 'Content-Type': 'application/json' },
 })
